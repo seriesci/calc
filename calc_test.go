8eae5feee7ab23f53c9fb3da433a6993f5831c38
Mirco Zeiss <mirco.zeiss@gmail.com>
2019-08-22 17:03:55 +0200

    inc coverage again

--- calc_test.go
+++ calc_test.go
@@ -11,16 +11,16 @@ func TestAdd(t *testing.T) {
 	}
 }
 
-// func TestSubtract(t *testing.T) {
-// 	result := Subtract(3,2)
-// 	if result != 1 {
-// 		t.Errorf("expected %d; got %d", 1, result)
-// 	}
-// }
+func TestSubtract(t *testing.T) {
+	result := Subtract(3,2)
+	if result != 1 {
+		t.Errorf("expected %d; got %d", 1, result)
+	}
+}
 
-// func TestMultiply(t *testing.T) {
-// 	result := Multiply(1,2)
-// 	if result != 2 {
-// 		t.Errorf("expected %d; got %d", 2, result)
-// 	}
-// }
+func TestMultiply(t *testing.T) {
+	result := Multiply(1,2)
+	if result != 2 {
+		t.Errorf("expected %d; got %d", 2, result)
+	}
+}
