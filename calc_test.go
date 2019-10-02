260490b7bd11da09eccbd52f2d0e3a4d3e996633
Mirco Zeiss <mirco.zeiss@gmail.com>
2019-10-02 09:37:47 +0200

    add test for divide function

--- calc_test.go
+++ calc_test.go
@@ -5,21 +5,28 @@ import (
 )
 
 func TestAdd(t *testing.T) {
-	result := Add(1,2)
+	result := Add(1, 2)
 	if result != 3 {
 		t.Errorf("expected %d; got %d", 3, result)
 	}
 }
 
 func TestSubtract(t *testing.T) {
-	result := Subtract(3,2)
+	result := Subtract(3, 2)
 	if result != 1 {
 		t.Errorf("expected %d; got %d", 1, result)
 	}
 }
 
 func TestMultiply(t *testing.T) {
-	result := Multiply(1,2)
+	result := Multiply(1, 2)
+	if result != 2 {
+		t.Errorf("expected %d; got %d", 2, result)
+	}
+}
+
+func TestDivide(t *testing.T) {
+	result := Divide(2, 1)
 	if result != 2 {
 		t.Errorf("expected %d; got %d", 2, result)
 	}
